c6ae55a033e3440b5a4a7810392ae589d5825415
cherry347 <StillLoriLeigh@gmail.com>
2016-04-13 22:38:09 +0000

    loops1

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-//victim's info
-var howMany= prompt("How many victims do you have?");
-var victimIndex= [];
-for(var i=0; i < parseInt(howMany); i++) {
-    var victim = [
-    prompt("Please enter the name of the victim"),
-    prompt("Please enter their phone number"),
-    prompt("What is their street location?")
-];
-victimIndex.push(victim);
+// increaseing by: 100
+for(var i=0; i< 600; i += 100) {
+    console.log(i);
 }
-//volunteer info
-var help= prompt("How many volunteers are there?");
-var volunteerIndex=[];
-for(var i=0; i< parseInt(help); i++) {
-    var volunteer= [
-    prompt("Please enter the name of the volunteer"),
-    prompt("Please enter their phone number"),
-    prompt("What is their street location?")
-];
-volunteerIndex.push(volunteer);
-};
+// x2
+for(var i=1; i< 64; i= i*2){
+    console.log(i);
+}
+//triple
+function three (x) {
+    var count = 1;
+    for(var i = 0; i < x; i++){
+        console.log(count, count, count);
+        count = count + 1;
+    }
+}
+three(3);
+// +2
+for(var i=0; i< 12; i += 2) {
+console.log(i);
+}
+// add three 
+for(var i=0; i<18; i+= 3) {
+console.log(i);
+}
+// -1 
+for(var i= 9; i> -1; i -= 1) {
+    console.log(i);
+}
+// 0-3 (3 times)
+for(var j=0; j<3; j++){
+	for(var i=0; i<4; i++) {
+		console.log(i);
+	}
+}
+
