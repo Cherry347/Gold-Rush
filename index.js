044040a929c0ee240df98550b9d9a50514c40a69
cherry347 <StillLoriLeigh@gmail.com>
2016-04-15 19:45:31 +0000

    JS5: Edit in Place

--- index.js
+++ index.js
@@ -1,36 +1,7 @@
-// increaseing by: 100
-for(var i=0; i< 600; i += 100) {
-    console.log(i);
+for(var i=0; i<=2; i++){
+    var x= document.querySelectorAll(".click")[i];
+    console.log(x);
+    x.addEventListener("click", function(){
+        this.innerHTML= this.contentEditable="true";
+    });
 }
-// x2
-for(var i=1; i< 64; i= i*2){
-    console.log(i);
-}
-//triple
-function three (x) {
-    var count = 1;
-    for(var i = 0; i < x; i++){
-        console.log(count, count, count);
-        count = count + 1;
-    }
-}
-three(3);
-// +2
-for(var i=0; i< 12; i += 2) {
-console.log(i);
-}
-// add three 
-for(var i=0; i<18; i+= 3) {
-console.log(i);
-}
-// -1 
-for(var i= 9; i> -1; i -= 1) {
-    console.log(i);
-}
-// 0-3 (3 times)
-for(var j=0; j<3; j++){
-	for(var i=0; i<4; i++) {
-		console.log(i);
-	}
-}
-
