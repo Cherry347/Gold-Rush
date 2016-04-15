aca39e2ec9c4cd45568b2dee17a0193e3cf13973
cherry347 <StillLoriLeigh@gmail.com>
2016-04-15 21:38:30 +0000

    Problem Set #1

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-for(var i=0; i<=2; i++){
-    var x= document.querySelectorAll(".click")[i];
-    console.log(x);
-    x.addEventListener("click", function(){
-        this.innerHTML= this.contentEditable="true";
-    });
+var letterCapitalize= function(word) {
+    return word[0].toUpperCase() + word.substring(1);
 }
+letterCapitalize("cherry")
+
+var wordCount= function(string) {
+    return string.split(" ").length;
+};
+wordCount("here a lot of words, not really, haha haha haha")
