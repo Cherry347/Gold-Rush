d9a37cdd1136387d8d59a719c9245ca533cfd8b8
cherry347 <StillLoriLeigh@gmail.com>
2016-04-20 17:45:07 +0000

    Intro to Angular

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-var letterCapitalize= function(word) {
-    return word[0].toUpperCase() + word.substring(1);
-}
-letterCapitalize("cherry")
+angular.module("firstApp",[])
+angular.module("firstApp").controller("controller1", ["$scope", function($scope) {
 
-var wordCount= function(string) {
-    return string.split(" ").length;
-};
-wordCount("here a lot of words, not really, haha haha haha")
+//part one
+    $scope.showMe= true;
+    $scope.button = function () {
+        $scope.showMe= !$scope.showMe;
+    };
+    $scope.listGone= true;
+    $scope.list= function() {
+        $scope.listGone= !$scope.listGone;
+    };
+
+//part two
+    $scope.pinkyPie=false;
+    $scope.pinkpara = function() {
+        $scope.pinkyPie=!$scope.pinkyPie;
+    };
+    $scope.askUser= function() {
+        var yesNo= confirm("You are leaving the Mr. Klaws's site, is that ok?");
+        if(yesNo === false) {
+            event.preventDefault();
+            event.target.remove();
+        }
+    };
+
+//part three
+    $scope.noPop= true;
+    $scope.popUp= function() {
+        $scope.noPop= !$scope.noPop;
+    };
+}]);
