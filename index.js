44b3b11593292d2a7a42dbe56f22dcfc1ec9b9a3
cherry347 <StillLoriLeigh@gmail.com>
2016-04-21 00:45:11 +0000

    goldrush

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
-angular.module("firstApp",[])
-angular.module("firstApp").controller("controller1", ["$scope", function($scope) {
-
-//part one
-    $scope.showMe= true;
-    $scope.button = function () {
-        $scope.showMe= !$scope.showMe;
-    };
-    $scope.listGone= true;
-    $scope.list= function() {
-        $scope.listGone= !$scope.listGone;
-    };
-
-//part two
-    $scope.pinkyPie=false;
-    $scope.pinkpara = function() {
-        $scope.pinkyPie=!$scope.pinkyPie;
-    };
-    $scope.askUser= function() {
-        var yesNo= confirm("You are leaving the Mr. Klaws's site, is that ok?");
-        if(yesNo === false) {
-            event.preventDefault();
-            event.target.remove();
-        }
-    };
-
-//part three
-    $scope.noPop= true;
-    $scope.popUp= function() {
-        $scope.noPop= !$scope.noPop;
-    };
-}]);
+angular.module("gold", []);
+angular.module('gold').controller('eureka', ["$scope", findGold]);
+function findGold($scope) {
+    $scope.pins =[];
+    $scope.drop = function() {
+        $scope.x=event.clientX;
+        $scope.y=event.clientY;
+        $scope.pins.push({'x': $scope.x, 'y': $scope.y});
+    }
+}
